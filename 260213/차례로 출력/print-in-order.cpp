61ff604ddc274b5fd2ae8755f4d59409fb17cc8d
Jjohunbaek <hoj0902@naver.com>
2026-02-13 10:30:54 +0900

    Title: 차례로 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260213/차례로 출력/print-in-order.cpp
+++ 260213/차례로 출력/print-in-order.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    for (int i = 5; i < 18; < i++) cout << i << " " ;
+    for (int i = 5; i < 18;  i++) cout << i << " " ;
 
     return 0;
 }
