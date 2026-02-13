7488818d1d841c949f832a858c03e051111359d5
Jjohunbaek <hoj0902@naver.com>
2026-02-13 10:04:48 +0900

    Title: 문자 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260213/문자 출력하기/print-text.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char ch;
+    cin >> ch;
+
+    for (int i =  0;i < 8; i++) cout << ch;
+    return 0;
+}
