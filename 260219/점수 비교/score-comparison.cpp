234fcf2024eb4f0e846962e57c5e6c3946115bab
Jjohunbaek <hoj0902@naver.com>
2026-02-19 12:16:24 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260219/점수 비교/score-comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a_m, a_e ,b_m, b_e;
+    cin >> a_m >> a_e;
+    cin >> b_m >> b_e;
+
+    if ((a_m > b_m) & (a_e > b_e)) cout << 1;
+    else cout << 0;
+    return 0;
+}
