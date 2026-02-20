2d9d42f76871d994c8c9455e3130e9a2b741865b
Jjohunbaek <hoj0902@naver.com>
2026-02-20 09:03:01 +0900

    Title: a로 채워넣기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260220/a로 채워넣기/filling-with-a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string s1, s2;
+    cin >> s;
+    s2 = s1[0] + "a" + s[2:];
+    cout << s2;
+    return 0;
+}
