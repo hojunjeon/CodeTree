09a259a1044ead71d7c60548d9fa39c1cfef5f4e
Jjohunbaek <hoj0902@naver.com>
2026-02-20 09:01:08 +0900

    Title: 더 긴 문자열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260220/더 긴 문자열/longer-string.cpp
+++ 260220/더 긴 문자열/longer-string.cpp
@@ -6,9 +6,9 @@ int main() {
     // Please write your code here.
     string s1, s2;
     cin >> s1 >> s2;
-    int l1 = s1.length()
-    int l2 = s2.length()
-    if (l1 > l2) cout << s1 << " "<< l1;
+    int l1 = s1.length();
+    int l2 = s2.length();
+    if (l1 > l2) cout << s1 << " "<< l1 ;
     else if (l1 == l2) cout << "same";
     else cout << s2 << " " << l2 ;
     return 0;
