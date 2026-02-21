0d7b8d8ac5b42f2881bde62055c8c4e4e6c01a46
Jjohunbaek <hoj0902@naver.com>
2026-02-21 15:35:09 +0900

    Title: 0 나올때까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 260221/0 나올때까지 출력/print-until-zero-is-given.cpp
+++ 260221/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    int n = 0;
+    int n;
+    cin >> n;
     while (n != 0){
-        cin >> n;
         cout << n <<endl;
+        cin >> n;
     }
     return 0;
 }
