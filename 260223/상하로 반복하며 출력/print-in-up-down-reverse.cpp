3e5f127a84c611aad8fe545aa71f9b32923601ae
Jjohunbaek <hoj0902@naver.com>
2026-02-23 16:21:01 +0900

    Title: 상하로 반복하며 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260223/상하로 반복하며 출력/print-in-up-down-reverse.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int arr[n][n];
+
+    for (int i=0;i<n;i++){
+        if (i % 2 == 0) for (int j = 0;j<n;j++) arr[j][i] = j + 1;
+        else for (int j = 0;j<n;j++) arr[j][i] = n - j;
+    }
+
+    for (int i=0;i<n;i++){
+        for (int j = 0;j < n; j ++) cout << arr[i][j];
+        cout << endl;
+    }
+    return 0;
+}
