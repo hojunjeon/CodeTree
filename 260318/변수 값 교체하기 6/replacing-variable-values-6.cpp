e1045ce469dcd0788364d0b90134968439ec9371
Jjohunbaek <hoj0902@naver.com>
2026-03-18 10:08:38 +0900

    Title: 변수 값 교체하기 6, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260318/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 2, b = 5;
+    int temp = a;
+    a = b;
+    b = temp;
+
+    cout << a << '\n' << b;
+    return 0;
+}
