d5a624e85f0f44dee54552dfde0415db551e7e23
jjohunbaek <hoj0902@naver.com>
2026-04-08 12:47:32 +0900

    Title: 국영수 순이지, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,23 @@
+n = int(input())
+name = []
+korean = []
+english = []
+math = []
+
+for _ in range(n):
+    student_info = input().split()
+    name.append(student_info[0])
+    korean.append(int(student_info[1]))
+    english.append(int(student_info[2]))
+    math.append(int(student_info[3]))
+
+# Please write your code here.
+students = []
+for i in range(n):
+    temp = (name[i], korean[i], english[i], math[i])
+    students.append(temp)
+
+students.sort(key=lambda x:(-x[1], -x[2], -x[3]))
+
+for s in students:
+    print(*s)
